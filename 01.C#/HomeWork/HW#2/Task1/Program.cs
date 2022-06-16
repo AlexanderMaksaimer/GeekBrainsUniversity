34aa6debea6f63a5f3672af989035c85eaaf7983
«Aleksander <pavel4uk18@gmail.com>
2022-06-16 19:10:03 +0300

    HW2Rdy

---
+++ 01.C#/HomeWork/HW#2/Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на ввод трехзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Введите любое трехзначное число: ");
+string NumberStr = Console.ReadLine();
+int number = int.Parse(NumberStr);
+string strr = number.ToString();
+Console.WriteLine($"{number} -> {strr[1]}");
