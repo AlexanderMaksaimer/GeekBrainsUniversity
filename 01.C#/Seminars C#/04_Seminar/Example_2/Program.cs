747430bade371c9e3607aa123cdc1532a7a096d0
«Aleksander <pavel4uk18@gmail.com>
2022-06-17 08:01:27 +0300

    Seminar4Rdy

---
+++ 01.C#/Seminars C#/04_Seminar/Example_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+
+string GetNumber ()
+{
+    return Console.ReadLine();
+}
+
+int CountNum(string str)
+{
+    int num = int.Parse(str);
+    int count = 0;
+    //int? count;
+
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+//int CountNum(string str)
+//{
+//    return str.Length;
+//}
+
+Console.WriteLine("Введите число: ");
+int countNum = CountNum(GetNumber());
+
+Console.WriteLine(countNum);
