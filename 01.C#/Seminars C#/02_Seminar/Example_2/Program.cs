b4654edd9e37edc2a39f98bf08d5c421eb2bdcb7
«Aleksander <pavel4uk18@gmail.com>
2022-06-17 07:53:16 +0300

    Seminar2Rdy

---
+++ 01.C#/Seminars C#/02_Seminar/Example_2/Program.cs
@@ -0,0 +1,5 @@
+﻿// Вывести на экран первую и третью цифру из трехзначного числа
+int number = new Random ().Next(100,1000);
+string strr = number.ToString();
+
+Console.WriteLine($"{number} -> {strr[0]}{strr[2]}");
