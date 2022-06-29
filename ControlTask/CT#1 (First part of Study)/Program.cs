a6fba4ce7d8a358f5fe509483ef3260f23a23271
«Aleksander <pavel4uk18@gmail.com>
2022-06-29 09:19:42 +0300

    StartCodeCommit

--- ControlTask/CT#1 (First part of Study)/Program.cs
+++ ControlTask/CT#1 (First part of Study)/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+
+["1234", "1567", "-2", "computer sience"] -> ["-2"]
+
+["Russia","Denmark", "Kazan"] ->[]*/
+
+// 1. метод создания массива строк
+// 2. метод создания нового массива с определенной длиной (например <=3 символа)
+// 3. вывод первого и второго массива на консоль
+
+string[] ArrayCreation()//Метод создания массива 
+{
+    Console.WriteLine("Введите эллементы массива - после ввода каждого элемента - нажмите любую кнопку для ввода следующего элемента - или нажмите ESC для завершения создания массива");
+    int index = 0;
+    string text = String.Empty;
+    List<string> arrayList = new List<string>();
+    do
+    {
+        Console.Write($"Эллемент массива № {index} : ");
+        text = Console.ReadLine();
+        if (text == null) text = String.Empty;// проверка пустых значений
+        arrayList.Add(text);
+        index++;
+        Console.WriteLine("\tДля продолжения ввода элементов нажмите любую кнопку, для выхода нажмите ESC");
+    }
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+    string[] array = new string[arrayList.Count];
+
+    for (int i = 0; i < array.Length; i++) array[i] = arrayList[i];
+
+    return array;
+}
+
