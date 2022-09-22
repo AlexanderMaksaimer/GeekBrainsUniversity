9b4e888f0b2460beb279e76b17e32f5c74210154
«Aleksander <pavel4uk18@gmail.com>
2022-09-22 13:55:47 +0300

    Семинар №1 Java

---
+++ 04.JAVA/1.Lessons/java_projects/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        float f = 123.45f;
+        System.out.println(f); // печать в консоль 
+    
+    }
+    
+}
---
+++ 04.JAVA/2.Seminars/01.Seminar/Main.java
@@ -0,0 +1,84 @@
+// import java.util.ArrayList;
+// import java.util.Random;
+
+// public class Main { // точка входа
+    // public static void main(String[] args) { // сигнатура (Метод/Функция)
+    //     System.out.println("Hello World"); //sout (Print)
+    //     int x = 10;//основные примитивные типы
+    //     long y = 476438756;
+    //     float f = 3.454f;
+    //     double d = 3.7d;
+
+    //     char ch; // хранит индекс буквы в таблице букв
+    //     byte bt = -12;
+    //     short sh = 456;
+
+    //     boolean b = false;
+    //     String str = "string это тоже класс"; //(ссылочный тип) класс, который внутри хранит массив Чаров. Функционал открывается через точку (str.)
+
+    //     System.out.println(str.length());
+
+    //     String [] strings = {"a","b","s"}; //массивы статические (если сказали 7 элементов, то 8 не засунуть)
+    //     int [] ints = new int [10]; //массив с 0 до 9
+
+    //     //Main main = new Main(); // вызов (создание/инициализация) объекта(экземпляра) этого класса 
+
+    //     if (b | x =='e'){ //возвращает Истину
+    //         System.out.println("3+1");
+    //     }
+
+    //     while(x > 0){
+    //         System.out.println("While");
+    //         x = x + 1;
+    //     }
+
+    //     do {
+    //         System.out.println("do");
+    //     } while (x ++ < 5);
+
+    //         //счетчик |  условие выхода| что будет происходить на итерации (например увеличение счетчика)   
+    //     for (int i = 0; i < ints.length; i++) {
+    //         System.out.println("for");
+    //         break; //работает break для выхода тоже
+    //     }
+
+    //     switch (str) {
+    //         case "a":
+    //             System.out.println("a"); 
+    //             break;
+    //         case "b":
+    //             System.out.println("b");
+    //             break;
+    //         default:
+    //     }
+    //     int [] intss = new int[10];
+    //     int [] tmp = new int[intss.length+1];
+    //     for (int i = 0; i < tmp.length; i++) {
+    //         tmp[i] = intss[i];
+    //     }
+    //     tmp[tmp.length-1] = 5;
+    //     intss = tmp;
+
+    //     Integer j;
+    //     Integer.max(1, 2);
+
+        // ArrayList<Integer> array = new ArrayList();
+        // for (int i = 0; i < 500; i++) {
+        //     array.add(int)(Math.random()*Integer.MAX_VALUE);            
+        // }
+
+        // ArrayList arrayList = new ArrayList();
+        // arrayList.add("Hello");
+        // arrayList.add(5);
+        // arrayList.add(5.543f);
+        
+        
+    
+
+//     } // тело метода Main
+
+//     public static String sayHi(String name) {return "Hi" + name + "!";
+// }    
+//  //тело класса (код внутри писать нельзя)
+
+// }
